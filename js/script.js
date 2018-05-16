425fec5d4af0ac71dc7887976d3005992268fc22
natalka77712 <kostousik@mail.ru>
2018-05-16 15:20:50 +0300

    pixel perfect

---
+++ js/script.js
@@ -0,0 +1,21 @@
+  var search = document.querySelector(".button-hotels");
+  var popup = document.querySelector(".search-form");
+  var arrival-date = document.querySelector("[name=arrival-date]");
+  var departure-date = document.querySelector("[name=departure-date]");
+  var adults = document.querySelector("[name=adults]");
+  var children = document.querySelector("[name=children]");
+  
+    popup.classList.add("search-form-show");
+  
+  search.addEventListener("click", function () {
+  console.log("Клик по кнопке поиска гостиниц");
+       evt.preventDefault();
+    popup.classList.toggle("search-form-show");
+});
+
+  popup.addEventListener("submit", function () {
+  if (!arrival-date.value || !departure-date.value || !adults.value
+   || !children.value ) {
+  evt.preventDefault();
+}
+});
